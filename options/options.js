7d40320d56a82d89c4899905176590483f23931b
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-07-31 16:54:38 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "july31-dev-105703",
-  url: "https://july31-dev-105703.botics.co",
-  api: "https://july31-dev-105703.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "july31-dev-105703",
+  "url": "https://july31-dev-105703.botics.co",
+  "api": "https://july31-dev-105703.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "signin"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
